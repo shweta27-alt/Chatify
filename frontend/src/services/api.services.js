217edef0648182686c4847ac9292c0176bb7fc18
Shweta Rajendra Saindane <80962110+shweta27-alt@users.noreply.github.com>
2024-05-27 00:25:45 +0530

    vercel added

--- frontend/src/services/api.services.js
+++ frontend/src/services/api.services.js
@@ -6,7 +6,7 @@ const getHeader = () => {
 };
 
 const getUrl = () => {
-  return "https://chatify-api-shweta27alts-projects.vercel.app/";
+  return "https://chatify-api-shweta27alts-projects.vercel.app";
 };
 
 // api service to contain all api
